818f6488ee573d4ffd15c9508952dfad05f59d6e
abh33 <abhinavsarkar2012@gmail.com>
2015-05-11 12:20:28 +0530

    CODE COMPLETED

--- mycode.cpp
+++ mycode.cpp
@@ -5,19 +5,15 @@ int main() {
 
   cout << "(x^0)/0! + (x^1)/1! + (x^2)/2! + (x^3)/3! + (x^4)/4! + ........ + (x^n)/n! " << endl;
 
-  cout << "Please enter the value of x : " ;
-
-  double x;
-  cin >> x;
-
   int n;
-  cout << endl << "Please enter an integer value for n : " ;
-  cin >> n;
-  cout << endl;
+  double x;
+  cout<<"eNTER X:";
+  cin>>x;
+  cout<<"Enter n:";
+  cin>>n;
 
-  double seriesValue = ComputeSeriesValue(x, n);
-  cout << "The value of the series for the values entered is "
-	<< seriesValue << endl;
-
+  double res=ComputeSeriesValue(x,n);
+  cout<<"\nRESULT:"<<res;
+	cout<<"I REALLY DONT KNOW WUT AM I DOINg HERE BUT IT SEEMS FUN"	;
   return 0;
 }
